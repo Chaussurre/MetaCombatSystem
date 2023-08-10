8b48cf3669597ef86be2bbdbad9cf1fbe4629edb
Chaussurre <samuel@corno.com>
2023-08-10 10:35:09 +0200

    Statuses are sources

--- Assets/MetaCombatSystem/StatusManager/StatusAlteration.cs
+++ Assets/MetaCombatSystem/StatusManager/StatusAlteration.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 namespace MetaCombatSystem.StatusManagement
 {
     [CreateAssetMenu(fileName = "StatusAlteration", menuName = "MetaCombat System/Status Alteration")]
-    public class StatusAlteration : ScriptableObject
+    public class StatusAlteration : ScriptableObject, ICombatSystemSource
     {
         public StatusAlterationEffect EffectPrefab;
         public string StatusName;
